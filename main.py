a55a5041df8851c1fb5c3764d1135d3427d54b8c
chris22763 <cmuehrke@me.com>
2018-11-14 23:37:51 +0100

    first pseudo code

---
+++ main.py
@@ -0,0 +1,21 @@
+
+try:
+    import flask
+    import cv2
+    import numpy
+    import midi #  ??
+except Exception as e:
+    print(e)
+
+def create_server():
+    pass
+
+def minmaxpull(img, ocimgsize, neimgsize):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+        main()
