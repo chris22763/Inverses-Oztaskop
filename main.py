b26f97bd5f0af0910871c1f1a5dc44a743135864
chris22763 <cmuehrke@me.com>
2018-11-26 21:21:36 +0100

    output
    Assertion fctx->async_lock failed at libavcodec/pthread_frame.c:155

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import os
 import random
 import midi
-import cv2 as cv
+import cv2
 import numpy as np
 from os import listdir
 from os.path import isfile, join
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for, session, escape
+import multiprocessing
+
+
+
+PATH = "./static/video_files/CGI_Animated_Short_Film_The_Box_La_Boîte_by_ESMA_CGMeetup.mp4"
+
+NSIZE =  40
+OCIMGX = 1280
+OCIMGY = 720
+NEIMGX = int(OCIMGX / NSIZE)
+NEIMGY = int(OCIMGY / NSIZE)
+
+OCIMGSIZE = (OCIMGX, OCIMGY)
+NEIMGSIZE = (NEIMGX, NEIMGY)
 
 app = Flask(__name__, static_url_path='/static')
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -14,22 +28,11 @@ app.config.from_object(__name__)
 app.secret_key = os.urandom(24)
 
 
-NSIZE =  40
-OCIMGX = 1920
-OCIMGY = 1080
-NEIMGX = OCIMGX / NSIZE
-NEIMGY = OCIMGY / NSIZE
-
-OCIMGSIZE = (OCIMGX, OCIMGY)
-NEIMGSIZE = (NEIMGX, NEIMGY)
-
-
-def webserver_init():
-    pass
+cap = cv2.VideoCapture(PATH)
+loaded = False
 
 def resize_frame(img, nsize, imgSize, output=0, resize=False): #output: 0 => max/ 1 => min):
-    cropedimg = np.zeros((NEIMGX,NEIMGY))
-
+    cropedimg = np.zeros((NEIMGY, NEIMGX))
 
     for x in range(NEIMGX):
         for y in range(NEIMGY):
@@ -41,7 +44,10 @@ def resize_frame(img, nsize, imgSize, output=0, resize=False): #output: 0 => max
 
             mask = img[y0:y1, x0:x1]
             minMax = cv2.minMaxLoc(mask)
-            val = 1 / minMax[1]
+            if minMax[0] != 0:
+                val = 1 / minMax[0]
+            else:
+                val = 0
 
             cropedimg[y][x] = val
 
@@ -49,10 +55,9 @@ def resize_frame(img, nsize, imgSize, output=0, resize=False): #output: 0 => max
 
 
 def get_ch_from_frame(img):
-    r, g, b, gray = 0
 
-    b, g, r = cv.split(img)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    b, g, r = cv2.split(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     gray = resize_frame(gray, NEIMGSIZE, OCIMGSIZE)
     r = resize_frame(r, NEIMGSIZE, OCIMGSIZE)
@@ -61,43 +66,48 @@ def get_ch_from_frame(img):
 
     return r, g, b, gray
 
-#todo make this shit work
-def get_frames(cap):
-    count = 0
-    data = []
-    if not cap.isOpened():
-        print("Error opening video stream or file")
+def output_list(output, value):
 
-    while cap.isOpened():
-        if count > 500:
-            break
-        if ret:
+    output.append(value)
 
-            data.append(img)  # save frame as JPEG file
-            ret, img = cam.read()
-            count += 1
+    return output
 
-    cam.release()
-
-    return data
-
-def send_data(data, ip, port):
+def sound_out():
     pass
 
 def convert_data():
-    pass
+
+    c = 0
+    out = []
+    while True:
+        try:
+            ret, frame = cap.read()
+
+            c += 1
+            if c == 90:
+                r, g, b, k = get_ch_from_frame(frame)
+                c = 0
+                out = output_list(out, (r, g, b, k))
+                #cv2.imshow("k", k)
+
+
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+        except:
+            break
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-
+    global loaded
     session['user_id'] = str(random.randint(0, 1000000000))
 
+    convert_data()
 
     if request.method == 'POST':
-        print(request.form)
+        pass
 
-    print("test")
     return render_template('index.html')
 
 
