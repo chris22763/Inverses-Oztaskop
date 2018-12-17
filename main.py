c40b40217780a49383131272552053c58fbd6858
chris22763 <cmuehrke@me.com>
2018-12-17 21:12:32 +0100

    wup wup

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ video_clips = {
     'luxor_jr': ["./static/video_files/short_test.mp4", 30, 96, 1184, 720],
     'game': ["./static/video_files/game.mp4", 24, 272, 1280, 720]
 }
+data = {
+    0: 'pre rendered data array',
+    1: [[[84, 129, 177], [21, 61, 103], [9, 16, 20], [77, 124, 0], [126, 0, 50]], [[103, 0, 36], [9, 33, 58], [10, 10, 9], [39, 70, 120], [59, 103, 0]]],
+    2: [[[78, 155, 184], [112, 152, 0], [9, 11, 11], [156, 0, 63], [14, 67, 120]], [[141, 0, 71], [16, 70, 109], [7, 8, 9], [111, 142, 0], [87, 147, 175]], [[113, 147, 0], [98, 163, 189], [9, 12, 14], [20, 77, 116], [148, 0, 75]], [[145, 0, 71], [8, 11, 12], [96, 160, 187], [19, 74, 115], [110, 145, 0]], [[9, 12, 14], [147, 0, 74], [98, 160, 187], [19, 74, 116], [111, 146, 0]]] ,
+    3: [[[46, 17, 5], [126, 85, 64], [3, 10, 3], [0, 97, 63], [10, 0, 52]], [[5, 5, 8], [137, 130, 61], [64, 42, 9], [53, 0, 99], [0, 88, 94]], [[44, 141, 0], [9, 83, 185], [2, 13, 99], [1, 2, 5], [142, 0, 6]]],
+
+}
+paths = {
+    0: './static/video_files/short_test.mp4',
+    1: './static/video_files/short_test.mp4',
+    2: './static/video_files/game_short.mp4',
+    3: './static/video_files/blade_runner_short.mp4'
+}
+
 
 OCIMGX = 1280
 OCIMGY = 720
@@ -38,7 +52,6 @@ app.config.from_object(__name__)
 app.secret_key = os.urandom(24)
 
 
-cap = cv2.VideoCapture(PATH)
 loaded = False
 
 def resize_frame(img): #output: 0 => max/ 1 => min):
@@ -76,8 +89,11 @@ def combine_img_part(new_img, last_img):
     return vis
 
 
-def convert_data():
+def convert_data(i):
 
+    global paths
+
+    _cap = cv2.VideoCapture(paths[i])
     c = 0
     fps = 24
     time_period = 8
@@ -89,7 +105,7 @@ def convert_data():
         start = time.time()
         c += 1
         try:
-            ret, frame = cap.read()
+            ret, frame = _cap.read()
 
             img = resize_frame(frame)
 
@@ -128,6 +144,7 @@ def index():
     global loaded
 
     sound = []
+    video_path = ''
 
     session['user_id'] = str(random.randint(0, 1000000000))
 
@@ -135,14 +152,21 @@ def index():
     #convert_data()
 
     if request.method == 'POST':
+        p = request.form
+        print(p)
         if loaded:
-            sound  = convert_data()
+            if p['select'] != '0':
+                i = int(p['select'])
+                #sound = data[i]
+                sound  = convert_data(i)
+                video_path = paths[i]
+
             #p = multiprocessing.Pool(1)
             #result = p.apply_async(convert_data)
 
         loaded = True
 
-    return render_template('index.html', sound_list=sound)
+    return render_template('index.html', sound_list=sound, video_path=video_path)
 
 
 
