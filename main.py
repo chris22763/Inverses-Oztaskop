47989199203468ddbd4b45af8f1c1895db0b2321
chris22763 <cmuehrke@me.com>
2019-01-08 11:21:58 +0100

    comment

--- main.py
+++ main.py
@@ -14,26 +14,20 @@ import matplotlib.pyplot as plt
 
 PATH = "./static/video_files/short_test.mp4"
 
-
-video_clips = {
-    'short_test' : ["./static/video_files/short_test.mp4", 24, 16, 1280, 720],
-    'luxor_jr': ["./static/video_files/short_test.mp4", 30, 96, 1184, 720],
-    'game': ["./static/video_files/game.mp4", 24, 272, 1280, 720]
-}
+#pre rendered data array
 data = {
-    0: 'pre rendered data array',
-    1: [[[84, 129, 177], [21, 61, 103], [9, 16, 20], [77, 124, 0], [126, 0, 50]], [[103, 0, 36], [9, 33, 58], [10, 10, 9], [39, 70, 120], [59, 103, 0]]],
-    2: [[[180, 161, 0], [161, 0, 206], [0, 0, 0], [209, 183, 163], [0, 206, 181]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[148, 142, 0], [70, 161, 149], [8, 7, 7], [5, 67, 145], [142, 0, 65]], [[9, 69, 129], [126, 187, 0], [186, 0, 65], [75, 156, 219], [0, 1, 2]], [[12, 9, 8], [69, 129, 175], [119, 0, 48], [14, 46, 79], [79, 121, 0]]],
-    3: [[[46, 17, 5], [126, 85, 64], [3, 10, 3], [0, 97, 63], [10, 0, 52]], [[5, 5, 8], [137, 130, 61], [64, 42, 9], [53, 0, 99], [0, 88, 94]], [[44, 141, 0], [9, 83, 185], [2, 13, 99], [1, 2, 5], [142, 0, 6]]],
-    4: [[[125, 121, 0], [9, 57, 105], [6, 6, 6], [61, 150, 144], [124, 0, 46]], [[57, 50, 0], [13, 56, 49], [1, 1, 0], [0, 13, 55], [48, 0, 14]], [[123, 116, 0], [111, 142, 132], [13, 13, 14], [15, 92, 113], [114, 0, 91]], [[14, 46, 65], [11, 9, 9], [59, 132, 139], [99, 0, 58], [89, 94, 0]], [[12, 9, 8], [59, 126, 134], [97, 0, 57], [13, 44, 63], [91, 97, 0]]]
+    0: [[[84, 129, 177], [21, 61, 103], [9, 16, 20], [77, 124, 0], [126, 0, 50]], [[103, 0, 36], [9, 33, 58], [10, 10, 9], [39, 70, 120], [59, 103, 0]]],
+    1: [[[180, 161, 0], [161, 0, 206], [0, 0, 0], [209, 183, 163], [0, 206, 181]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[148, 142, 0], [70, 161, 149], [8, 7, 7], [5, 67, 145], [142, 0, 65]], [[9, 69, 129], [126, 187, 0], [186, 0, 65], [75, 156, 219], [0, 1, 2]], [[12, 9, 8], [69, 129, 175], [119, 0, 48], [14, 46, 79], [79, 121, 0]]],
+    2: [[[46, 17, 5], [126, 85, 64], [3, 10, 3], [0, 97, 63], [10, 0, 52]], [[5, 5, 8], [137, 130, 61], [64, 42, 9], [53, 0, 99], [0, 88, 94]], [[44, 141, 0], [9, 83, 185], [2, 13, 99], [1, 2, 5], [142, 0, 6]]],
+    3: [[[125, 121, 0], [9, 57, 105], [6, 6, 6], [61, 150, 144], [124, 0, 46]], [[57, 50, 0], [13, 56, 49], [1, 1, 0], [0, 13, 55], [48, 0, 14]], [[123, 116, 0], [111, 142, 132], [13, 13, 14], [15, 92, 113], [114, 0, 91]], [[14, 46, 65], [11, 9, 9], [59, 132, 139], [99, 0, 58], [89, 94, 0]], [[12, 9, 8], [59, 126, 134], [97, 0, 57], [13, 44, 63], [91, 97, 0]]]
 
 }
+# path for the clips
 paths = {
-    0: 'path for the clips',
-    1: './static/video_files/short_test.mp4',
-    2: './static/video_files/game_short.mp4',
-    3: './static/video_files/blade_runner.mp4',
-    4: './static/video_files/LOTR.mp4'
+    0: './static/video_files/short_test.mp4',
+    1: './static/video_files/game_short.mp4',
+    2: './static/video_files/blade_runner.mp4',
+    3: './static/video_files/LOTR.mp4'
 }
 
 
@@ -95,19 +89,22 @@ def convert_data(i):
 
     global paths
 
-    _cap = cv2.VideoCapture(paths[i])
+    # initalizing cap
+    cap = cv2.VideoCapture(paths[i])
+
+    # c => is a counter we use (with fps) to determine if the time_period of 8 seconds has past
     c = 0
     fps = 24
     time_period = 8
+
+    # cache and vis are lists to store
     cache = np.zeros((NEIMGY, NEIMGX, 4), 'uint8')
     vis = np.zeros((NEIMGY, NEIMGX, 4), 'uint8')
     output = []
-
     while True:
-        start = time.time()
         c += 1
         try:
-            ret, frame = _cap.read()
+            ret, frame = cap.read()
 
             img = resize_frame(frame)
 
@@ -134,10 +131,6 @@ def convert_data(i):
             print("finish")
             break
 
-        end = time.time()
-
-        #print(end-start)
-
 
     return output
 
@@ -151,24 +144,20 @@ def index():
     session['user_id'] = str(random.randint(0, 1000000000))
 
 
-    #convert_data()
-
     if request.method == 'POST':
         p = request.form
-        #print(p)
-        #print(loaded)
+
         if p['select'] != '0':
             i = int(p['select'])
+
+            # convert_data loads a video clip with the path of index i mentioned in the list 'Path'
+            # the output data was stored in the list 'data' so on presentation we could skip the calculation step
+            # if someone want to try it for them self you just need to uncomment line 157 and comment line 159
+
+            # sound  = convert_data(i)
+
             sound = data[i]
-            print(sound)
-
-            #sound  = convert_data(i)
             video_path = paths[i]
-
-        #p = multiprocessing.Pool(1)
-        #result = p.apply_async(convert_data)
-
-        loaded = True
 
     return render_template('index_page.html', sound_list=sound, video_path=video_path)
 
